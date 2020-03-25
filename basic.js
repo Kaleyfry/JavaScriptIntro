a81ebfd318030f1f5613ffbd047fb4e7b08783ad
Kaleyfry <59694186+Kaleyfry@users.noreply.github.com>
2020-03-25 11:40:35 -0400

    Notes for intro to Javascript

---
+++ basic.js
@@ -0,0 +1,47 @@
+var amountRaisedSoFar = 1000;
+var newDonation = prompt("How much would you like to donate?");
+amountRaisedSoFar = Number(newDonation) + amountRaisedSoFar;
+console.log("We have now raised: " + amountRaisedSoFar + "!");
+
+
+
+
+// var numberOfCups = prompt("How many cups");
+// if (Number(numberOfCups < 3)){
+// console.log("Yes I'll take another cup of coffee")
+// } else {
+// console.log("I think I'm okay for now")
+// }
+
+// alert("Thank you! Your input has been recorded.")
+
+
+// var numberOfCups = 4;
+// if (numberOfCups < 3){
+// console.log("Yes I'll take another cup of coffee")
+// } else {
+// console.log("I think I'm okay for now")
+// }
+
+// var temp = 80;
+// var precipitation = false; //"none"
+// var indoors = true;
+// console.log("The temperature is " + temp + " degrees");
+// if (temp > 80 && precipitation!==true) {
+//     //if (temp > 80 && precipitation === "none")
+//     console.log("time to swim")
+// } else if (indoors) {
+//     console.log("time to swim")
+// } else {
+//     console.log("Don't swim outdoors with percipitation")
+// }
+
+// var temp = 81;
+// var tempMessage = "The temperature is " + temp + " degrees"
+// console.log(tempMessage)
+// var precip = "raining"
+// var indoors = false
+
+// if (indoors || (temp > 80 && precip === "none")) {
+//     console.log('time to swim')
+// }
